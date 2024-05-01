710c94216a312fb9023aa7541426d254fe67e2be
SamridhG <samridh1998@gmail.com>
2024-05-02 02:21:20 +0530

    Card Added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { render } from "react-dom";
 import ReactDom from 'react-dom/client'
+import MeghnaFood from '../texture/MeghnaFood.jpg'
+import list from '../src/json/fooditem.json'
 const Logo=()=>{
     return(
         <div className="logo-container">
@@ -28,10 +29,50 @@ const HeaderParent=()=>{
 <NavBar/>
     </div>)
 }
+const MenuText=()=>{
+    return (
+        <div className="menu-title">
+            <div className="title-img"></div>
+            <div className="menu">Our Menu</div>
+        </div>
+    )
+}
+const Card=(details)=>{
+    const item=details.food
+    console.log("Details",item)
+   return ( <div className="card-parent">
+            <div className="card-img">
+            <img src={item.img}></img>
+            </div>
+            <div className="name-and-price">
+                <div className="card-name">{item.name}</div>
+                <div className="card-price">{item.price}</div>
+            </div>
+            <div className="card-discription">{item.description}</div>
+    </div>)
+}
 
+const CardConatainer=()=>{
+    return (
+        <div className="card-container-parent">
+        <div className="card-container">
+           {
+            list.map((item)=>(
+                <Card key={item.id} food={item}/>
+            ))
+           }
+         </div>
+        </div>
+    )
+} 
 const AppLayout=()=>{
     return (
-        <HeaderParent/>
+        <div className="app">
+            <HeaderParent/>
+            <MenuText/>
+            <CardConatainer/>
+        </div>
+        
     )
 }
 const root=ReactDom.createRoot(document.getElementById("root"))
